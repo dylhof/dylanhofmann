89f857162c33d462ad80bdca490f6907d09a9268
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-07-10 16:06:57 -0600

    Add Project styles

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 export const Project = ({ title, git, url, description, thumbnail}) => {
   return(
-    <div>
+    <div className='project-div'>
       <h2>{title}</h2>
-      <img src={thumbnail}/>
-      <p>{description}</p>
+      <img src={thumbnail} alt={title}/>
+      <p className='project-description-p'>{description}</p>
       <p>gitHub link: <a href={git} target='blank'>{git}</a></p>
       <p>Live link: <a href={url} target='blank'>{url}</a></p>
     </div>
