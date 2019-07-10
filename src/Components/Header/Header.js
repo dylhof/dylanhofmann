0efd041a6616a1f41449e39434eb979458536ba2
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-07-10 16:50:00 -0600

    Add subtitle and styles

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export const Header = () => {
   return(
-    <header>
+    <header className='Header-div'>
       <h1>Dylan Hofmann</h1>
+      <h2>Front End Software Developer</h2>
       <p>dylan.hofmann@gmail.com</p>
     </header>
   )
