03ee6c0878c26e1b639505adab9d6529f4052f9e
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-08-26 16:36:13 -0600

    Add Router into App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import ProjectArea from '../ProjectArea/ProjectArea';
 import {Header} from '../Header/Header';
 import {AboutMe} from '../AboutMe/AboutMe';
-
+import {ContactMe} from '../ContactMe/ContactMe';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component{
   
   render() {
     return (
-      <div className="App">
-        <Header/>
-        <AboutMe/>
-        <ProjectArea/>
-      </div>
+      <Router>
+        <div className='App'>
+          <Header/>
+          <Route path='/' exact render={() => (
+            <div>
+              <AboutMe/>
+              <ProjectArea/>
+            </div>)}
+          />
+          <Route path='/contactme' component={ContactMe} />
+        </div>
+      </Router>
     );
   }
 }
